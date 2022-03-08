289a75e14eb5bf04cf959169ab8978ca29951e42
MicaG28 <65852191+MicaG28@users.noreply.github.com>
2022-03-08 23:24:04 +0000

    admin v1

---
+++ Bubble/admin/index.php
@@ -0,0 +1,13 @@
+<?php
+include('./partials/header.php');
+
+?>
+
+
+
+
+
+<?php
+include('./partials/footer.php');
+
+?>
---
+++ Bubble/admin/partials/header.php
@@ -0,0 +1,21 @@
+<?php
+
+// conection check
+include('./db_con.php')
+
+// middleware redirect
+
+
+//
+
+?>
+
+<html lang="pt-pt" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title>Admin</title>
+
+    <script src="../public/js/jquery.min.js"></script>
+</head>
+<body>
+
