aaa916935504edec6a272a2d6f92015300708bba
Assado3000 <dev.andre.sousa@gmail.com>
2022-03-10 11:50:51 +0000

    update

---
+++ Bubble/footer.php
@@ -0,0 +1,12 @@
+<footer>
+
+</footer>
+
+<!--Scripts-->
+<script src="https://kit.fontawesome.com/27dd9727ef.js" crossorigin="anonymous"></script>
+<script src="jquery/jquery-3.6.0.min.js"></script>
+<script src="js/header.js"></script>
+
+</body>
+
+</html>
--- Bubble/mensagens.php
+++ Bubble/mensagens.php
@@ -1,23 +1,5 @@
-<!DOCTYPE html>
-<html lang="pt-PT">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--<link rel="shortcut icon" type="image/jpg" href="img/logo.ico"/>-->
-    <!--CSS-->
-    <link rel="stylesheet" href="css/mensagens.css">
-    <link rel="stylesheet" href="css/header.css">
-    <!--Google Fonts-->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;1,100;1,200&display=swap" rel="stylesheet">
-</head>
-<body>
-    
-<?php include 'header.php';?>
+<?php include 'header.php'; ?>
 
-<noscript>Por Favor ative o JavaScript nas definições do seu Browser. Pode consultar como o fazer clicando <a href="https://support.google.com/adsense/answer/12654" target="_blank">aqui</a> </noscript>
 <div class="conteudo">
     <div class="barra-listagem-mensagens">
         <div class="pesquisa-fixed">
@@ -37,12 +19,12 @@
 
         <div class="listagem-chats">
 
-        <div class="wrap-pessoa">
-            <div class="foto-perfil">
+            <div class="wrap-pessoa">
+                <div class="foto-perfil">
+
+                </div>
 
             </div>
-
-        </div>
 
         </div>
 
@@ -57,9 +39,5 @@
     </div>
 
 </div>
-<!--Scripts-->
-<script src="https://kit.fontawesome.com/27dd9727ef.js" crossorigin="anonymous"></script>
-<script src="jquery/jquery-3.6.0.min.js"></script>
-<script src="js/header.js"></script>
-</body>
-</html>
+
+<?php include 'footer.php'; ?>
