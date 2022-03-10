4079b173f06f0582d0163763f4038b86015a66c8
Assado3000 <dev.andre.sousa@gmail.com>
2022-03-10 01:13:41 +0000

    fe

--- Bubble/header.php
+++ Bubble/header.php
@@ -1,7 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-PT">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--<link rel="shortcut icon" type="image/jpg" href="img/logo.ico"/>-->
+    <!--CSS-->
+    <link rel="stylesheet" href="css/mensagens.css">
+    <link rel="stylesheet" href="css/header.css">
+    <!--Google Fonts-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;1,100;1,200&display=swap" rel="stylesheet">
+    
+    <?php 
+
+    $pagina = basename($_SERVER["REQUEST_URI"]);
+
+    $nomePagina = "";
+
+    if ($pagina == 'mensagens.php') { 
+
+        $nomePagina = "ZAZA";
+
+    } else if ($pagina == 'index.php') {
+
+    }
+
+    ?>
+
+  <title>Bubbles | <?php echo $nomePagina?> </title>
+
+</head>
+
+<body>
 
     <nav>
         <div id="nav">
-            <a href="feed.html"><img class="logo" src="Untitled-2.png" alt="logo"></a>
+            <a href="feed.html"><img class="logo" src="img/header/Untitled-2.png" alt="logo"></a>
             <span id="space"></span>
             <ul class="navbar">
                 <li>Feed</li>
@@ -11,14 +48,14 @@
             </ul>
             <div class="icon_perfil">
                 <div class="perfil">
-                    <img class="img_perfil" src="download.png" alt="fotoperfil">
+                    <img class="img_perfil" src="img/header/download.png" alt="fotoperfil">
                     <p>Joãozinho</p>
                 </div>
             </div>
         </div>
         <div class="popup_perfil">
             <ul>
-                <li><a href="feed.html"><img src="download.png" alt="fotoperfil">
+                <li><a href="feed.html"><img src="img/header/download.png" alt="fotoperfil">
                         <div id="ver_perfil"><span>Joãozinho</span><span id="ver_perfil_span">Ver Perfil</span>
                         </div>
                     </a>
--- Bubble/mensagens.php
+++ Bubble/mensagens.php
@@ -12,7 +12,6 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;1,100;1,200&display=swap" rel="stylesheet">
-    <title>Bubbles | Mensagens</title>
 </head>
 <body>
     
@@ -21,26 +20,32 @@
 <noscript>Por Favor ative o JavaScript nas definições do seu Browser. Pode consultar como o fazer clicando <a href="https://support.google.com/adsense/answer/12654" target="_blank">aqui</a> </noscript>
 <div class="conteudo">
     <div class="barra-listagem-mensagens">
+        <div class="pesquisa-fixed">
 
-        <div class="inicio-chats">
-            <h2><strong>Chats</strong></h2>
-            <div class="novo-chat"><i class="fa-solid fa-plus"></i></div>
+            <div class="inicio-chats">
+                <h2><strong>Chats</strong></h2>
+                <div class="novo-chat"><i class="fa-solid fa-plus"></i></div>
+            </div>
+
+            <div class="pesquisa-mensagens">
+                <form class="form-pesquisa" action="#">
+                    <input class="pesquisa" type="search" placeholder="Pesquisar Conversas..." name="search">
+                </form>
+            </div>
+
         </div>
 
-        <div class="pesquisa-mensagens">
-            <form class="form-pesquisa" action="#">
-                <input class="pesquisa" type="search" placeholder="Pesquisar Conversas..." name="search">
-              </form>
-        </div>
-        
+        <div class="listagem-chats">
+
         <div class="wrap-pessoa">
             <div class="foto-perfil">
 
             </div>
-            <div >
 
-            </div>
         </div>
+
+        </div>
+
     </div>
 
     <div class="wrap-conteudo-mensagens">
@@ -54,5 +59,7 @@
 </div>
 <!--Scripts-->
 <script src="https://kit.fontawesome.com/27dd9727ef.js" crossorigin="anonymous"></script>
+<script src="jquery/jquery-3.6.0.min.js"></script>
+<script src="js/header.js"></script>
 </body>
 </html>
