4480831134bcad2768980af355529c48bea9eb12
MicaG28 <submica99@gmail.com>
2022-03-13 19:42:21 +0000

    ladmn

---
+++ Bubble/admin/login.php
@@ -0,0 +1,43 @@
+<html lang="pt-pt" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title>Admin</title>
+
+    <link rel="stylesheet" href="./public/bootstrap-5.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./public/css/styles.css">
+    <link rel="stylesheet" href="./public/fontawesome-6.0.0/css/all.css">
+    <script src="./public/js/jquery.min.js"></script>
+    <script src="./public/js/app.js"></script>
+</head>
+<body>
+
+<main class="login">
+
+    <div class="left flex align-content-center justify-content-center">
+        <img src="./public/img/logo.png" alt="Logo">
+    </div>
+
+    <div class="right col-12">
+        <form class="form-login col-12" action="dologin.php">
+            <div class="col-12">
+                <div class="col-12">
+                    <label class="col-12" for="email">Email</label>
+                    <input class="border" name="email" type="email" id="email">
+                </div>
+
+                <div class="col-12">
+                    <label class="col-12" for="password">Password</label>
+                    <input class="border" name="password" type="password" id="password">
+                </div>
+            </div>
+        </form>
+    </div>
+
+</main>
+
+
+</body>
+</html>
+
+
+
