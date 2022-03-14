e907e941618879ab1407f7454028a5ef5979b0d7
João Duarte <joao.bartolomeu.tic@gmail.com>
2022-03-14 16:46:16 +0000

    kkk

--- DWM_2021_22-Prj1_Bubble/Site/eventos.php
+++ DWM_2021_22-Prj1_Bubble/Site/eventos.php
@@ -5,15 +5,44 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/eventos.css">
     <link rel="shortcut icon" href="img/logo.ico" type="image/x-icon">
     <title>Bubble | Eventos</title>
 
 </head>
 
 <body>
+<?php include 'header.php'; ?>
 
-    
+<div class="home">
+
+<div class="left">
+        <ul>
+            <li><a class="verperfil" href="perfil.php"><img class="avatar" src="img/header/download.png"
+                        alt="fotoperfil">Joãozinho Mineiro</a>
+            </li>
+            <li><a href=""><i class='bx bx-group'></i>Amigos</a></li>
+            <li><a href=""><i class='bx bx-hash'></i>Tópicos</a></li>
+            <li><a href=""><i class='bx bx-buildings'></i>Empresas</a></li>
+            <li><a href=""><i class='bx bxs-megaphone'></i>Oferta de Emprego</a></li>
+            <li><a href=""><i class='bx bx-calendar-event'></i>Eventos</a></li>
+            <li><a href=""><i class='bx bx-question-mark'></i>FAQS</a></li>
+        </ul>
+    </div>
+
+<div class="caixas-eventos1">
+    <div class="caixas" >
+        <div id="imagem-caixa">   </div>
+        <div id="descricao-caixa">  </div>
+
+    </div>
+  
+</div>
+
+
+
+</div>
+
 
 
     <script src="js/eventos.js"></script>
