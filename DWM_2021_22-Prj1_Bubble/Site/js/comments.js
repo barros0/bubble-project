fb63c2243e566dc63d47b38efdc1cd2385aa5574
barros0 <joaoaobarros7@gmail.com>
2022-03-15 21:09:34 +0000

    update

--- DWM_2021_22-Prj1_Bubble/Site/js/comments.js
+++ DWM_2021_22-Prj1_Bubble/Site/js/comments.js
@@ -1,31 +1,8 @@
-/*$(document).ready(function () {
+$(document).ready(function () {
   $(".comment_textarea").each(function () {
     $(this).keyup(function () {
       var len = $(this).val().length;
-      var a = $(this).find("span").text(len)
+      $(".current_chars").text(len);
     });
   });
 });
-*/
-/*
-$(document).ready(function () {
-  $(".comment_section").each(function () {
-    $(this).addid("dwa");
-    $(".comment_textarea").each(function () {
-      $(this).keyup(function () {
-        var len = $(this).val().length;
-        $(".dwa").find("span").text(len);
-      });
-    });
-  });
-});
-
-$(document).ready(function () {
-    
-    for (let i = 0; i < array.length; i++) {
-        $(".blah").eq(i).html("blah");
-    }
-});
-
-*/
-
