d9716edcef1b5502b3899bf04065d3d5cc378ad4
MicaG28 <submica99@gmail.com>
2022-03-17 09:47:55 +0000

    aa

--- DWM_2021_22-Prj1_Bubble/admin/users/users.php
+++ DWM_2021_22-Prj1_Bubble/admin/users/users.php
@@ -57,6 +57,44 @@ include('../partials/header.php');
                 </span>
                 </td>
                 <td><i class="fa fa-pen"></i></td>
+            </tr><tr>
+                <th scope="row">1</th>
+                <td>
+                    <img class="user-img" src="https://thispersondoesnotexist.com/image" alt="">
+                    <span>aaaaa</span>
+                </td>
+                <td>aaaaa</td>
+                <td>
+                <span class="mini-card bg-warning">
+                    Bloqueado
+                </span>
+                </td>
+                <td><i class="fa fa-pen"></i></td>
+            </tr><tr>
+                <th scope="row">1</th>
+                <td>
+                    <img class="user-img" src="https://thispersondoesnotexist.com/image" alt="">
+                    <span>aaaaa</span>
+                </td>
+                <td>aaaaa</td>
+                <td>
+                <span class="mini-card bg-warning">
+                    Bloqueado
+                </span>
+                </td>
+                <td><i class="fa fa-pen"></i>
+                    <div class="dropdown show">
+                        <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown link
+                        </a>
+
+                        <div class="dropdown-menu show" aria-labelledby="dropdownMenuLink">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </div>
+                </td>
             </tr>
 
             </tbody>
