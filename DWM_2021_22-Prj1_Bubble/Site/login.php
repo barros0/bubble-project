b28eedce53bd773a6d23f97cc6ee5e2c283018dd
barros0 <joaoaobarros7@gmail.com>
2022-03-31 10:12:50 +0100

    update

--- DWM_2021_22-Prj1_Bubble/Site/login.php
+++ DWM_2021_22-Prj1_Bubble/Site/login.php
@@ -1,105 +1,86 @@
-<!DOCTYPE html>
-<html lang="en">
+<div class="home">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/login.css">
-    <link rel="shortcut icon" href="img/header/logo_small_bubble.ico" type="image/x-icon">
-    <title>Bubble | Login</title>
+    <div class="home2">
 
-</head>
-
-<body>
-
-    <div class="home">
-
-        <div class="home2">
-
-            <div class="caixa1-home">
+        <div class="caixa1-home">
 
 
-                <img class='logo-caixa' src="img/header/logo_bubble.svg" alt="logo">
+            <img class='logo-caixa' src="img/header/logo_bubble.svg" alt="logo">
 
 
+        </div>
+
+        <div class="caixa2-home">
+
+            <div class="botao-caixa">
+                <div id="fundo-botoes"></div>
+                <button type="button" class="botoes-form" onclick="login()">Login</button>
+                <button type="button" class="botoes-form" onclick="register()">Sign Up</button>
             </div>
 
-            <div class="caixa2-home">
+            <form method="POST" id="login" action="formulario" class="formulario-login">
 
-                <div class="botao-caixa">
-                    <div id="fundo-botoes"></div>
-                    <button type="button" class="botoes-form" onclick="login()">Login</button>
-                    <button type="button" class="botoes-form" onclick="register()">Sign Up</button>
+                <input type="email" class="form-input" name="email" placeholder="E-Mail" required>
+                <input type="text" class="form-input" name="nome" placeholder="Password" required>
+                <label id="checkbox">
+
+                    <input type="checkbox" class="check-box">
+                    Manter Sessão Iniciada
+                </label>
+                <button type="submit" name="entrarconta" class="botao-submeter">Iniciar Sessão</button>
+
+            </form>
+
+            <form method="POST" id="register" action="formulario" class="formulario-login">
+
+                <div class="label-nomes">
+                    <input type="text" id="label-nome" class="form-input" name="nome" placeholder="Nome" required>
+                    <input type="text" class="form-input" name="sobrenome" placeholder="Sobrenome" required>
                 </div>
+                <input type="email" class="form-input" name="email" placeholder="E-Mail" required>
+                <input type="text" class="form-input" name="telemovel" placeholder="Número de Telemóvel" required>
+                <div class="label-passwords">
+                    <input type="text" id="label-password" class="form-input" name="password" placeholder="Password"
+                        required>
+                    <input type="text" id="label-password" class="form-input" name="password1"
+                        placeholder="Confirmar Password" required>
+                </div>
+                <input type="date" class="form-input" name="data" placeholder="Data de Nascimento" required>
+                <label for="sexo">O seu género é:</label>
+                <select name="sexo" class="form-input" id="sexo">
+                    <option value="masculino">Masculino</option>
+                    <option value="feminino">Feminino</option>
+                    <option value="Outro">Outro</option>
+                </select>
+                <label id="checkbox">
+                    <input type="checkbox" class="check-box">
+                    Concorda com os nossos <span> Termos & Condições</span>?
+                </label>
 
-                <form method="POST" id="login" action="formulario" class="formulario-login">
+                <button type="submit" name="registarconta" class="botao-submeter">Regista-te</button>
 
-                    <input type="email" class="form-input" name="email" placeholder="E-Mail" required>
-                    <input type="text" class="form-input" name="nome" placeholder="Password" required>
-                    <label id="checkbox">
+            </form>
 
-                        <input type="checkbox" class="check-box">
-                        Manter Sessão Iniciada
-                    </label>
-                    <button type="submit" name="entrarconta" class="botao-submeter">Iniciar Sessão</button>
-
-                </form>
-
-                <form method="POST" id="register" action="formulario" class="formulario-login">
-
-                    <div class="label-nomes">
-                        <input type="text" id="label-nome" class="form-input" name="nome" placeholder="Nome" required>
-                        <input type="text" class="form-input" name="sobrenome" placeholder="Sobrenome" required>
-                    </div>
-                    <input type="email" class="form-input" name="email" placeholder="E-Mail" required>
-                    <input type="text" class="form-input" name="telemovel" placeholder="Número de Telemóvel" required>
-                    <div class="label-passwords">
-                        <input type="text" id="label-password" class="form-input" name="password" placeholder="Password"
-                            required>
-                        <input type="text" id="label-password" class="form-input" name="password1"
-                            placeholder="Confirmar Password" required>
-                    </div>
-                    <input type="date" class="form-input" name="data" placeholder="Data de Nascimento" required>
-                    <label for="sexo">O seu género é:</label>
-                    <select name="sexo" class="form-input" id="sexo">
-                        <option value="masculino">Masculino</option>
-                        <option value="feminino">Feminino</option>
-                        <option value="Outro">Outro</option>
-                    </select>
-                    <label id="checkbox">
-                        <input type="checkbox" class="check-box">
-                        Concorda com os nossos <span> Termos & Condições</span>?
-                    </label>
-
-                    <button type="submit" name="registarconta" class="botao-submeter">Regista-te</button>
-
-                </form>
-
-
-            </div>
-        </div>
-
-        <div class="footer">
-
-            <div id="footer-informacoes"><a href="feed.php">Feed</a><a href="faqs.html"> FAQs</a><a
-                    href="ajuda.html">Ajuda</a><a href="termos.html">Termos</a><a href="definicoes.html">Definições</a>
-            </div>
-            <div id="footer-copyright">© 2022 Bubble. All rights reserved</div>
 
         </div>
+    </div>
 
+    <div class="footer">
+
+        <div id="footer-informacoes"><a href="feed.php">Feed</a><a href="faqs.html"> FAQs</a><a
+                href="ajuda.html">Ajuda</a><a href="termos.html">Termos</a><a href="definicoes.html">Definições</a>
+        </div>
+        <div id="footer-copyright">© 2022 Bubble. All rights reserved</div>
 
     </div>
 
 
+</div>
 
 
-    <script src="js/login.js"></script>
 
-</body>
 
-</html>
+<script src="js/login.js"></script>
 
 
 <!--LOGIN !-->
