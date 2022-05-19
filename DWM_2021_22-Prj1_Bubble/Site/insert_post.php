31d44360ebebbef5948b6c5183392d578b99e2e7
barros0 <joaoaobarros7@gmail.com>
2022-05-19 10:54:34 +0100

    Update insert_post.php

--- DWM_2021_22-Prj1_Bubble/Site/insert_post.php
+++ DWM_2021_22-Prj1_Bubble/Site/insert_post.php
@@ -1,8 +1,10 @@
 <?php
+require('./bd.php');
+session_start();
 $texto = $_REQUEST['text'];
 $imagem = $_REQUEST['file'];
 
-$sql = "INSERT INTO publicacoes  VALUES ('$texto','$imagem')";
+$sql = "INSERT INTO publicacoes (user_id,conteudo,estado) VALUES (1,'".$texto."',1)";
 
-
+$conn->query($sql);
 ?>
