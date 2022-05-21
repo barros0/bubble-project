e864eaa923ed6d22ed1a69d62a66f2d99dfa4d1d
Assado3000 <dev.andre.sousa@gmail.com>
2022-05-21 22:42:35 +0100

    wqertyu
    dftdyguy

--- DWM_2021_22-Prj1_Bubble/Site/mensagens.php
+++ DWM_2021_22-Prj1_Bubble/Site/mensagens.php
@@ -22,217 +22,217 @@ $fotos = ""; //src de fotos enviadas
 ?>
 
 <div class="conteudo">
-    
-    <div class="barra-listagem-mensagens">
+    <div class="barratopo">
+        <div class="barra-listagem-mensagens">
 
-    <div class="listagem-chats">
+            <div class="listagem-chats">
 
-<!--Novo Chat--->
-<div class="wrap-pessoa">
-    <div class="foto-perfil-container">
-        <div class="foto-perfil">
-            <div class="novo-chat"><i class="fa-solid fa-plus"></i></div>
-        </div>
-    </div>
-</div>
+                <!--Novo Chat--->
+                <div class="wrap-pessoa">
+                    <div class="foto-perfil-container">
+                        <div class="foto-perfil">
+                            <div class="novo-chat"><i class="fa-solid fa-plus"></i></div>
+                        </div>
+                    </div>
+                </div>
 
-<!--Pesquisa--->
+                <!--Pesquisa--->
 
-<div class="wrap-pessoa">
-    <div class="foto-perfil-container">
-        <div class="foto-perfil">
-            <div class="novo-chat"><i class="fas fa-search"></i></div>
-        </div>
-    </div>
-</div>
+                <div class="wrap-pessoa">
+                    <div class="foto-perfil-container">
+                        <div class="foto-perfil">
+                            <div class="novo-chat"><i class="fas fa-search"></i></div>
+                        </div>
+                    </div>
+                </div>
 
-<div class="wrap-pessoa">
-    <div class="foto-perfil-container">
-        <div class="foto-perfil">
-            <img src="img/header/download.png" alt="Foto de Perfil">
-        </div>
-    </div>
-</div>
+                <div class="wrap-pessoa">
+                    <div class="foto-perfil-container">
+                        <div class="foto-perfil">
+                            <img src="img/header/download.png" alt="Foto de Perfil">
+                        </div>
+                    </div>
+                </div>
 
-<div class="wrap-pessoa">
-    <div class="foto-perfil-container">
-        <div class="foto-perfil">
-            <img src="img/header/download.png" alt="Foto de Perfil">
-        </div>
-    </div>
-</div>
+                <div class="wrap-pessoa">
+                    <div class="foto-perfil-container">
+                        <div class="foto-perfil">
+                            <img src="img/header/download.png" alt="Foto de Perfil">
+                        </div>
+                    </div>
+                </div>
 
-<div class="wrap-pessoa">
-    <div class="foto-perfil-container">
-        <div class="foto-perfil">
-            <img src="img/header/download.png" alt="Foto de Perfil">
-        </div>
-    </div>
-</div>
+                <div class="wrap-pessoa">
+                    <div class="foto-perfil-container">
+                        <div class="foto-perfil">
+                            <img src="img/header/download.png" alt="Foto de Perfil">
+                        </div>
+                    </div>
+                </div>
 
-<div class="wrap-pessoa">
-    <div class="foto-perfil-container">
-        <div class="foto-perfil">
-            <img src="img/header/download.png" alt="Foto de Perfil">
-        </div>
-    </div>
-</div>
+                <div class="wrap-pessoa">
+                    <div class="foto-perfil-container">
+                        <div class="foto-perfil">
+                            <img src="img/header/download.png" alt="Foto de Perfil">
+                        </div>
+                    </div>
+                </div>
 
-<div class="wrap-pessoa">
-    <div class="foto-perfil-container">
-        <div class="foto-perfil">
-            <img src="img/header/download.png" alt="Foto de Perfil">
-        </div>
-    </div>
-</div>
+                <div class="wrap-pessoa">
+                    <div class="foto-perfil-container">
+                        <div class="foto-perfil">
+                            <img src="img/header/download.png" alt="Foto de Perfil">
+                        </div>
+                    </div>
+                </div>
 
-</div>
+            </div>
 
-<div class="pesquisa-fixed">
+            <div class="pesquisa-fixed">
 
-            <div class="pesquisa-mensagens">
-                <form class="form-pesquisa" action="#" method="POST">
-                    <div class="fundo-pesquisa"><input class="pesquisa" type="search"
-                            placeholder="Pesquisar Conversas..." name="search"></div>
-                </form>
+                <div class="pesquisa-mensagens">
+                    <form class="form-pesquisa" action="#" method="POST">
+                        <div class="fundo-pesquisa"><input class="pesquisa" type="search" placeholder="Pesquisar Conversas..." name="search"></div>
+                    </form>
+                </div>
+
             </div>
 
         </div>
+    </div>
 
-</div>
+
+
+
+
+        <div class="wrap-conteudo-mensagens">
+
+            <div class="conteudo-mensagens">
+
+                <div class="conteudo-chat">
+
+                    <div class="row-mensagem">
+
+                        <div class="icone-perfil-row-mensagem">
+
+                            <div class="foto-perfil">
+                                <img src="img/header/download.png" alt="Foto de Perfil">
+                            </div>
+
+                        </div>
+
+                        <div class="conteudo-row-mensagem">
+                            <span><?php echo $testeMensagem ?></span>
+                        </div>
+
+                    </div>
+
+                    <div class="row-mensagem">
+
+                        <div class="conteudo-row-mensagem enviada">
+                            <span><?php echo $testeMensagem ?></span>
+                        </div>
+
+                        <div class="icone-perfil-row-mensagem">
+
+                            <div class="foto-perfil">
+                                <img src="img/header/download.png" alt="Foto de Perfil">
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div class="row-mensagem">
+
+                        <div class="icone-perfil-row-mensagem">
+
+                            <div class="foto-perfil">
+                                <img src="img/header/download.png" alt="Foto de Perfil">
+                            </div>
+
+                        </div>
+
+                        <div class="conteudo-row-mensagem">
+                            <span><?php echo $testeMensagem ?></span>
+                        </div>
+
+                    </div>
+
+                    <div class="row-mensagem">
+
+                        <div class="conteudo-row-mensagem enviada">
+                            <span><?php echo $testeMensagem ?></span>
+                        </div>
+
+                        <div class="icone-perfil-row-mensagem">
+
+                            <div class="foto-perfil">
+                                <img src="img/header/download.png" alt="Foto de Perfil">
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div class="row-mensagem">
+
+                        <div class="icone-perfil-row-mensagem">
+
+                            <div class="foto-perfil">
+                                <img src="img/header/download.png" alt="Foto de Perfil">
+                            </div>
+
+                        </div>
+
+                        <div class="conteudo-row-mensagem">
+                            <span><?php echo $testeMensagem ?></span>
+                        </div>
+
+                    </div>
+
+                    <div class="row-mensagem">
+
+                        <div class="conteudo-row-mensagem enviada">
+                            <span><?php echo $testeMensagem ?></span>
+                        </div>
+
+                        <div class="icone-perfil-row-mensagem">
+
+                            <div class="foto-perfil">
+                                <img src="img/header/download.png" alt="Foto de Perfil">
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+            </div>
+
+        </div>
 
         
+        <div class="escrever-mensagem">
+                    <div class="texto-mensagem">
+                        <form class="form-mensagem" action="#" method="POST">
+                            <textarea class="mensagem" type="text" placeholder="Escreva uma Mensagem..." name="mensagem"></textarea>
 
-
-
-
-    <div class="wrap-conteudo-mensagens">
-
-        <div class="conteudo-mensagens">
-
-            <div class="conteudo-chat">
-
-                <div class="row-mensagem">
-
-                    <div class="icone-perfil-row-mensagem">
-
-                        <div class="foto-perfil">
-                            <img src="img/header/download.png" alt="Foto de Perfil">
-                        </div>
+                        </form>
 
                     </div>
+                    <div class="conteudo-multimedia">
 
-                    <div class="conteudo-row-mensagem">
-                        <span><?php echo $testeMensagem ?></span>
-                    </div>
+                        <div class="icones-chat"><i class='bx bxs-send'></i></div>
 
-                </div>
-
-                <div class="row-mensagem">
-
-                    <div class="conteudo-row-mensagem enviada">
-                        <span><?php echo $testeMensagem ?></span>
-                    </div>
-
-                    <div class="icone-perfil-row-mensagem">
-
-                        <div class="foto-perfil">
-                            <img src="img/header/download.png" alt="Foto de Perfil">
-                        </div>
+                        <div class="icones-chat"><i class="fa-solid fa-plus"></i></div>
 
                     </div>
-
                 </div>
 
-                <div class="row-mensagem">
-
-<div class="icone-perfil-row-mensagem">
-
-    <div class="foto-perfil">
-        <img src="img/header/download.png" alt="Foto de Perfil">
     </div>
-
-</div>
-
-<div class="conteudo-row-mensagem">
-    <span><?php echo $testeMensagem ?></span>
-</div>
-
-</div>
-
-<div class="row-mensagem">
-
-<div class="conteudo-row-mensagem enviada">
-    <span><?php echo $testeMensagem ?></span>
-</div>
-
-<div class="icone-perfil-row-mensagem">
-
-    <div class="foto-perfil">
-        <img src="img/header/download.png" alt="Foto de Perfil">
-    </div>
-
-</div>
-
-</div>
-
-<div class="row-mensagem">
-
-<div class="icone-perfil-row-mensagem">
-
-    <div class="foto-perfil">
-        <img src="img/header/download.png" alt="Foto de Perfil">
-    </div>
-
-</div>
-
-<div class="conteudo-row-mensagem">
-    <span><?php echo $testeMensagem ?></span>
-</div>
-
-</div>
-
-<div class="row-mensagem">
-
-<div class="conteudo-row-mensagem enviada">
-    <span><?php echo $testeMensagem ?></span>
-</div>
-
-<div class="icone-perfil-row-mensagem">
-
-    <div class="foto-perfil">
-        <img src="img/header/download.png" alt="Foto de Perfil">
-    </div>
-
-</div>
-
-</div>  
-
-            </div>
-
-            <div class="escrever-mensagem">
-                <div class="texto-mensagem">
-                    <form class="form-mensagem" action="#" method="POST">
-                        <textarea class="mensagem" type="text" placeholder="Escreva uma Mensagem..."
-                            name="mensagem"></textarea>
-
-                    </form>
-
-                </div>
-                <div class="conteudo-multimedia">
-
-                    <div class="icones-chat"><i class='bx bxs-send'></i></div>
-
-                    <div class="icones-chat"><i class="fa-solid fa-plus"></i></div>
-
-                </div>
-            </div>
-
-        </div>
-
-    </div>
-
-</div>
 
 </div>
 
