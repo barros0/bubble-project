ecaa3f263edd8d27d0d6503fcc1b8fe59c45f6eb
Assado3000 <dev.andre.sousa@gmail.com>
2022-05-24 13:47:30 +0100

    tryuf
    tyu

--- DWM_2021_22-Prj1_Bubble/Site/faqs.php
+++ DWM_2021_22-Prj1_Bubble/Site/faqs.php
@@ -4,67 +4,66 @@ include 'page_parts/header.php';
 
 include 'page_parts/left.php';
 
-
 //buscar detalhes na base de dados
-
-//provavelmente esta parte vai ser em arrays
-
-$idFAQ = ""; //id da faq
-$titulofaq = ""; //questao da faq
-$conteudoFAQ = ""; //resposta, conteudo da faq
+$query = 'SELECT * FROM faqs';
+$lista_faqs = $conn->query($query);
 
 ?>
 
 <div class="wrap-conteudo">
-    <div class="conteudo">
-        <div class="caixa-pesquisa">
-            <form class="form-pesquisa" action="#" method="POST">
-                <div class="fundo-pesquisa"><input class="pesquisa" type="search" placeholder="Pesquisar..." name="search"></div>
-            </form>
+  <div class="conteudo">
+    <div class="caixa-pesquisa">
+      <form class="form-pesquisa" action="#" method="POST">
+        <div class="fundo-pesquisa"><input class="pesquisa" type="search" placeholder="Pesquisar..." name="search"></div>
+      </form>
+    </div>
+
+    <div class="wrap-acordiao">
+
+      <?php
+
+      while ($row = $lista_faqs->fetch_assoc()) {
+
+        //buscar dados
+        $row['id_faq'];
+        $row['pergunta'];
+        $row['resposta'];
+
+        echo '<div class="accordion-item">
+        <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+            ' .$row['pergunta']. '
+          </button>
+        </h2>
+        <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+          <div class="accordion-body">
+          ' .$row['resposta']. '
+          </div>
         </div>
+      </div>';
 
-        <div class="wrap-acordiao">
-         
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-        Accordion Item #1
-      </button>
-    </h2>
-    <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
-      <div class="accordion-body">
-        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-        Accordion Item #2
-      </button>
-    </h2>
-    <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
-      <div class="accordion-body">
-        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="panelsStayOpen-headingThree">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-        Accordion Item #3
-      </button>
-    </h2>
-    <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
-      <div class="accordion-body">
-        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
 
+      }
+
+      ?>
+
+<div class="accordion-item">
+        <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+            Accordion Item #1
+          </button>
+        </h2>
+        <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+          <div class="accordion-body">
+            <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+          </div>
         </div>
+      </div>
+
 
     </div>
+
+  </div>
 </div>
 
 <?php include 'page_parts/footer.php'; ?>
