56512ebb761b09771bc27583ed8e6b12cb98f3f9
barros0 <joaoaobarros7@gmail.com>
2022-05-29 23:55:47 +0100

    Update insert_post.php

--- DWM_2021_22-Prj1_Bubble/Site/insert_post.php
+++ DWM_2021_22-Prj1_Bubble/Site/insert_post.php
@@ -2,28 +2,25 @@
 require('./bd.php');
 session_start();
 
-//TEXTO DA PUBLICACAO PARA PUBLICACOES
-$texto = $_REQUEST['text'];
+
+$texto = $_REQUEST['text']; //TEXTO DA PUBLICACAO PARA PUBLICACOES
 
 $qpublicacao ="INSERT INTO publicacoes (user_id,conteudo,estado) VALUES ('".$_SESSION['user']['id_user']."','".$texto."',1)";
 $publicacao = $conn->query($qpublicacao);
 
 //ID DA PUBLICACAO
 $idpub = (mysqli_insert_id($conn));
-//print_r($idpub);
 
 //IMAGEM DA PUBLICACAO PARA PUBLICACOES_FOTOS
 $imagem = $_FILES['foto_public']['name'];
 $extensao = pathinfo($imagem, PATHINFO_EXTENSION);
 $folder = "img/publicacoes/";
-//MUDAR DE NOME DA FOTO
-$novo_ficheiro = sha1(microtime()) . "." . $extensao;
+$novo_ficheiro = sha1(microtime()) . "." . $extensao; //MUDAR DE NOME DA FOTO
 
 if (move_uploaded_file($_FILES['foto_public']['tmp_name'], $folder.$novo_ficheiro)) {
-    $publicacao_foto = "INSERT INTO publicacoes_fotos (publicacao_id,caminho) VALUES('".$idpub."','".$novo_ficheiro."')";
-    $conn->query($publicacao_foto);
+$publicacao_foto = "INSERT INTO publicacoes_fotos (publicacao_id,caminho) VALUES('".$idpub."','".$novo_ficheiro."')";
+$conn->query($publicacao_foto);
 }
 
 header('location:feed.php');
-
 ?>
