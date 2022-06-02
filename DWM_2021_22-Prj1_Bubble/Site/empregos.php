d9f915f1f81cfc82174b3668c974880a79bc7375
Assado3000 <dev.andre.sousa@gmail.com>
2022-06-02 23:19:36 +0100

    rd
    fed

--- DWM_2021_22-Prj1_Bubble/Site/empregos.php
+++ DWM_2021_22-Prj1_Bubble/Site/empregos.php
@@ -14,32 +14,54 @@ $lista_empregos = $conn->query($query);
 
         <div class="wrap-empregos">
 
-            <div class="caixa-emprego">
-                <div class="foto-emprego"><img class="ft-emprego" src="http://blog.adrianalombardo.com/wp-content/uploads/2019/08/como-encontrar-emprego.jpg" alt="Emprego"></div>
+            <?php
+
+            while ($row = $lista_empregos->fetch_assoc()) {
+
+                //buscar dados
+                
+                $row['categoria'];
+                $row['descricao'];
+                
+
+                echo '<div class="caixa-emprego">
+                <div class="foto-emprego"><img class="ft-emprego" src="' . $row['foto'] .'" alt="Emprego"></div>
                 <div class="conteudo-emprego">
                     <div class="wrap-dt-emp">
-                    <h3 class="nome-emp">Nome Emprego</h3>
-                    <div class="detalhes">
-                        <div class="ind-detalhes">
-                    <span>Localização:</span> 
-                    <span>Tipo:</span> 
-                    <span>Horários:</span> 
+                        <h3 class="nome-emp">'. $row['titulo'] .'</h3>
+                        <div class="detalhes">
+                            <div class="ind-detalhes">
+                                <span>Qualificações:</span>
+                                <span>Experiência:</span>
+                                <span>Vagas:</span>
+                                <span>Localização:</span>
+                                <span>Tipo:</span>
+                                <span>Horários:</span>
+                            </div>
+                            <div class="texto-detalhes">
+                                <span>'. $row['qualificacoes'] .'</span>
+                                <span>'. $row['experiencia'] .'</span>
+                                <span>'. $row['vagas'] .'</span>
+                                <span>'. $row['localizacao'] .'</span>
+                                <span>'. $row['tipo'] .'</span>
+                                <span>'. $row['horario'] .'</span>
+                            </div>
+                        </div>
                     </div>
-                    <div class="texto-detalhes">
-                    <span>Leiria</span>
-                    <span>Remoto</span>
-                    <span>Full-Time</span>
-                    </div>
-                    </div>
-                    </div>
-                    <div class="btn-wrp"> 
+                    <div class="btn-wrp">
                         <button type="button" class="btn-emprego">Ver Emprego</button>
                     </div>
-                
-            </div>
+
+                </div>
+
+            </div>';
+
+            }
+
+            ?>
+
 
         </div>
     </div>
-</div>
 
-<?php include 'page_parts/footer.php'; ?>
+    <?php include 'page_parts/footer.php'; ?>
