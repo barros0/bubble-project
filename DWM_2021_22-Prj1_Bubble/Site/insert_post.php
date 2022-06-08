a736711ec3a04f1f5b75b53353159ea250944ba8
barros0 <joaoaobarros7@gmail.com>
2022-06-08 18:47:45 +0100

    Update insert_post.php

--- DWM_2021_22-Prj1_Bubble/Site/insert_post.php
+++ DWM_2021_22-Prj1_Bubble/Site/insert_post.php
@@ -17,9 +17,6 @@ $extensao = pathinfo($imagem, PATHINFO_EXTENSION);
 $folder = "img/publicacoes/";
 $novo_ficheiro = sha1(microtime()) . "." . $extensao; //MUDAR DE NOME DA FOTO
 
-if (move_uploaded_file($_FILES['foto_public']['tmp_name'], $folder . $novo_ficheiro)) {
-    $publicacao_foto = "INSERT INTO publicacoes_fotos (publicacao_id,caminho) VALUES('" . $idpub . "','" . $novo_ficheiro . "')";
-    $conn->query($publicacao_foto);
 $upload_ok = 1;
 if(isset($imagem)){
 // Check if image file is a actual image or fake image
