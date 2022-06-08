88cec2c990a060613da764fcfe604263be9318ed
barros0 <joaoaobarros7@gmail.com>
2022-06-08 20:43:17 +0100

    up

--- DWM_2021_22-Prj1_Bubble/Site/estrutura_post.php
+++ DWM_2021_22-Prj1_Bubble/Site/estrutura_post.php
@@ -9,59 +9,65 @@ $result_set = $conn->query($query);
 
 
 <?php
-while ($pub = $result_set->fetch_assoc()) { 
+while ($pub = $result_set->fetch_assoc()) {
 
-    $imagemq = "select * from publicacoes_fotos where publicacao_id =".$pub['publicacao_id'];
+    $imagemq = "select * from publicacoes_fotos where publicacao_id =" . $pub['publicacao_id'];
     $imagem = $conn->query($imagemq)->fetch_assoc();
-    ?>
-<div class="post">
-    <div class="user_post_info">
-        <div class="post_user_avatar">
-            <img src="img/header/download.png" alt="foto_perfil_user">
-            <div class="post_user_info">
-                <p class="post_user_name"> <?php echo($pub['nome']) ?></p>
-                <p class="post_user_date">Publicado - 24/10/2022 15:55</p>
+?>
+    <div class="post">
+        <div class="user_post_info">
+            <div class="cabecalho_post">
+                <div class="post_user_avatar">
+                    <img src="img/header/download.png" alt="foto_perfil_user">
+                    <div class="post_user_info">
+                        <p class="post_user_name"> <?php echo ($pub['nome']) ?></p>
+                        <p class="post_user_date">Publicado - 24/10/2022 15:55</p>
+                    </div>
+                </div>
+                <div class="remover_publicacao">
+                    <div class="circles_edit"></div>
+                    <div class="circles_edit"></div>
+                    <div class="circles_edit"></div>
+                </div>
             </div>
-        </div>
-        <div class="post_text">
-            <?php echo($pub['conteudo']) ?>
-        </div>
-        <?php
-    if(!empty($imagem)){
-        ?>
-        <div class="post_user_img">
-            <img src="./img/publicacoes/<?php echo($imagem['caminho'])?>" alt="">
-        </div>
-        <?php
- }
-        ?>
-        <div class="post_number_likes_comments">
-            <i class='bx bx-heart' id="liked"></i>
-            <p><span id="number_likes"></span> Gostos</p>
-        </div>
-        <div class="post_like_comment_share">
-            <div id="like">
-                <i class='bx bx-heart' id="liked_info"></i>
-                <p>Gostar</p>
+            <div class="post_text">
+                <?php echo ($pub['conteudo']) ?>
             </div>
-            <div class="comment" id="comment">
-                <i class='bx bx-comment'></i>
-                <p>Comentar</p>
+            <?php
+            if (!empty($imagem)) {
+            ?>
+                <div class="post_user_img">
+                    <img src="./img/publicacoes/<?php echo ($imagem['caminho']) ?>" alt="">
+                </div>
+            <?php
+            }
+            ?>
+            <div class="post_number_likes_comments">
+                <i class='bx bx-heart' id="liked"></i>
+                <p><span id="number_likes"></span> Gostos</p>
             </div>
-            <div id="share">
-                <i class='bx bx-share'></i>
-                <p>Partilhar</p>
+            <div class="post_like_comment_share">
+                <div id="like">
+                    <i class='bx bx-heart' id="liked_info"></i>
+                    <p>Gostar</p>
+                </div>
+                <div class="comment" id="comment">
+                    <i class='bx bx-comment'></i>
+                    <p>Comentar</p>
+                </div>
+                <div id="share">
+                    <i class='bx bx-share'></i>
+                    <p>Partilhar</p>
+                </div>
             </div>
-        </div>
-        <div class="comment_section">
-            <form action="">
-                <textarea data-limit=255 maxlength="255" name="textarea" class="comment_textarea"
-                    placeholder="Comente Algo"></textarea>
-                <p class="comment_limit"> <span class="current_chars">0</span>/255</p>
-            </form>
+            <div class="comment_section">
+                <form action="">
+                    <textarea data-limit=255 maxlength="255" name="textarea" class="comment_textarea" placeholder="Comente Algo"></textarea>
+                    <p class="comment_limit"> <span class="current_chars">0</span>/255</p>
+                </form>
+            </div>
         </div>
     </div>
-</div>
 
 <?php
 }
