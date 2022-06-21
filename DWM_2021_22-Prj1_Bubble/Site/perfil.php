346d10b94ba62d027a2bbf1227387cbc452f643e
MicaG28 <65852191+MicaG28@users.noreply.github.com>
2022-06-21 12:12:38 +0100

    folow 1 prepare

--- DWM_2021_22-Prj1_Bubble/Site/perfil.php
+++ DWM_2021_22-Prj1_Bubble/Site/perfil.php
@@ -1,13 +1,32 @@
 <?php include 'page_parts/header.php'; ?>
 
+<?php
+
+if($_GET['userid']){
+    $user_perfil_id = $_GET['userid'];
+}
+else{
+    $user_perfil_id =  $_SESSION['user']['id_user'];
+}
+
+ $user_perfil = $conn->query('select * from users inner join nacionalidades 
+    on users.nacionalidade = nacionalidades.nacionalidade_id where users.id_user = ' . $user_perfil_id)->fetch_assoc();
+
+$count_seguidores = $conn->query('select count(*) from seguir where id_utilizador = ' . $user_perfil_id)->fetch_assoc();
+
+$count_aseguir = $conn->query('select count(*) from seguir where id_seguidor = ' . $user_perfil_id)->fetch_assoc();
+
+?>
+
+
 <!--PARTE DE CIMA DO PERFIL-->
 <div class="pagina_perfil">
     <div class="conteudo_pagina_perfil">
         <div class="fundo_perfil">
-            <img src="img/fotos_banner/<?php echo ($userq['banner_image']) ?>" alt="imagem_fundo">
+            <img src="img/fotos_banner/<?php echo ($user_perfil['banner_image']) ?>" alt="imagem_fundo">
             <div class="pagina_foto_perfil">
-                <img src="img/fotos_perfil/<?php echo ($userq['profile_image']) ?>" alt="foto_perfil">
-                <p><?php echo ($userq['nome']) ?></p>
+                <img src="img/fotos_perfil/<?php echo ($user_perfil['profile_image']) ?>" alt="foto_perfil">
+                <p><?php echo ($user_perfil['nome']) ?></p>
             </div>
         </div>
         <div class="espaco_info_buttons">
@@ -21,7 +40,7 @@
                     <p>FOLLOWERS</p>
                 </div>
                 <div class="bandeira">
-                    <span class="fi fi-<?= strtolower($userq['siglapais']) ?>"></span>
+                    <span class="fi fi-<?= strtolower($user_perfil['siglapais']) ?>"></span>
                 </div>
             </div>
             <div class="pagina_buttons">
@@ -43,16 +62,16 @@
             <div class="perfil_esquerda_baixo">
                 <div class="perfil_sobre">
                     <p class="titulo_perfil">Sobre Mim</p>
-                    <p class="texto_sobre_perfil"><?php echo ($userq['sobre']) ?></p>
+                    <p class="texto_sobre_perfil"><?php echo ($user_perfil['sobre']) ?></p>
                     <p class="titulo_perfil">Skills</p>
-                    <p class="texto_sobre_perfil"><?php echo ($userq['skills']) ?></p>
+                    <p class="texto_sobre_perfil"><?php echo ($user_perfil['skills']) ?></p>
                     <div class="sobre_alinhado">
                         <p class="titulo_perfil">Membro Desde</p>
-                        <p class="texto_sobre_perfil"><?php echo ($userq['created_at']) ?></p>
+                        <p class="texto_sobre_perfil"><?php echo ($user_perfil['created_at']) ?></p>
                     </div>
                     <div class="sobre_alinhado">
                         <p class="titulo_perfil">Idade</p>
-                        <p class="texto_sobre_perfil"><?php echo ($userq['data_nascimento']) ?></p>
+                        <p class="texto_sobre_perfil"><?php echo ($user_perfil['data_nascimento']) ?></p>
                     </div>
                 </div>
                 <div class="perfil_sobre">
@@ -73,11 +92,11 @@
         </div>
         <div class="sobre_perfil">
             <label for="sobre_perfil">Sobre ti:</label>
-            <textarea name="SobrePerfil" id="sobre_perfil"><?php echo ($userq['sobre']) ?></textarea>
+            <textarea name="SobrePerfil" id="sobre_perfil"><?php echo ($user_perfil['sobre']) ?></textarea>
         </div>
         <div class="linguagens_perfil">
             <label for="skills_perfil">Trabalhas com o que?</label>
-            <textarea name="skills_perfil" id="programas_perfil"><?php echo ($userq['skills']) ?></textarea>
+            <textarea name="skills_perfil" id="programas_perfil"><?php echo ($user_perfil['skills']) ?></textarea>
         </div>
         <div class="inserir_fotos">
             <div class="foto_perfil">
