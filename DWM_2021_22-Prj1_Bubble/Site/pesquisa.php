ec8be2aec26c1bb5d8f9c1bde20b116f371e5a76
MicaG28 <65852191+MicaG28@users.noreply.github.com>
2022-06-21 14:22:17 +0100

    p2

--- DWM_2021_22-Prj1_Bubble/Site/pesquisa.php
+++ DWM_2021_22-Prj1_Bubble/Site/pesquisa.php
@@ -7,7 +7,7 @@ $resultados = array();
 
 $search = $_GET['search'];
 
-$searchTermBits = [];
+$searchTermosBits = [];
 
 $addpesquisa = $conn->prepare("insert into historico_pesquisa (id_utilizador, pesquisa) VALUES (?, ?)");
 $addpesquisa->bind_param("is", $userq['id_user'], $search);
@@ -16,15 +16,28 @@ $addpesquisa->execute();
 
 
 $pesquisaTerms = explode(' ', $search);
-$searchTermBits = array();
+$searchTermosBits = array();
 foreach ($pesquisaTerms as $termo) {
     $termo = trim($termo);
     if (!empty($termo)) {
-        array_push($searchTermBits, "conteudo LIKE '%$termo%'");
+        array_push($searchTermosBits, "conteudo LIKE '%$termo%'");
     }
 }
 
-$publicacoes = $conn->query("SELECT * FROM publicacoes WHERE " . implode(' AND ', $searchTermBits) . " ");
+$publicacoes = $conn->query("SELECT * FROM publicacoes WHERE " . implode(' AND ', $searchTermosBits) . " ");
+
+$users = $conn->query("SELECT * FROM users WHERE " . implode(' AND ', $searchTermosBits) . " ");
+
+foreach ($publicacoes as $pub) {
+    $pub_ar = [
+        'titulo' => $pub['conteudo'],
+        'subtitulo' => $pub['conteudo'],
+        'tipo' => 'publicacao',
+        //'img' => './img/publicacoes/'.$conn->query('select * from publicacoes_fotos where publicacao_id ='.$pub['publicacao_id'])->fetch_assoc()['caminho'],
+        'link' => './publicacao?id='. $pub['publicacao_id'],
+    ];
+    array_push($resultados, $pub_ar);
+}
 
 ?>
 
@@ -38,7 +51,7 @@ $publicacoes = $conn->query("SELECT * FROM publicacoes WHERE " . implode(' AND '
             <?php
 
 
-            print_r($publicacoes->fetch_assoc());
+            print_r($resultados);
 
 
             ?>
