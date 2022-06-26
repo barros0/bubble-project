1393b9c400110c2070f5f3dc236dd53a854e03c7
barros0 <joaoaobarros7@gmail.com>
2022-06-26 18:52:15 +0100

    update

--- DWM_2021_22-Prj1_Bubble/Site/page_parts/left.php
+++ DWM_2021_22-Prj1_Bubble/Site/page_parts/left.php
@@ -10,10 +10,11 @@
             <li><a href="eventos.php"><i class='bx bx-calendar-event'></i>Eventos</a></li>
             <li><a href="faqs.php"><i class='bx bx-question-mark'></i>FAQS</a></li>
         </ul>
-        <div class="footer_left">
-            <a href="">Privacidade</a> | <a href="">Termos</a> | <a href="">Publicidade</a> | <a href="">Cookies</a>
-            <br>
-            Bubble © 2022
-        </div>
+
     </div>
+</div>
+<div class="footer_left">
+    <a href="">Privacidade</a> | <a href="">Termos</a> | <a href="">Publicidade</a> | <a href="">Cookies</a>
+    <br>
+    Bubble © 2022
 </div>
--- DWM_2021_22-Prj1_Bubble/Site/update_perfil.php
+++ DWM_2021_22-Prj1_Bubble/Site/update_perfil.php
@@ -17,24 +17,70 @@ if ($nacionalidade != 'None') {
     $updatenacionalidade = $conn->query($qupdatenacionalidade);
 }
 
+$uploadOk_perfil = 1;
+$error_perfil = "";
+
 //BUSCAR AS IMAGEMS DE PERFIL E BANNER
 if ($imagem_perfil != "") {
     $extensao_perfil = pathinfo($imagem_perfil, PATHINFO_EXTENSION);
     $folder_perfil = "img/fotos_perfil/";
     $novo_ficheiro_perfil = sha1(microtime()) . "." . $extensao_perfil;
-    if (move_uploaded_file($_FILES['FotoPerfil']['tmp_name'], $folder_perfil . $novo_ficheiro_perfil)) {
-        $qeditarfotoperfil = "UPDATE users SET profile_image = '$novo_ficheiro_perfil' WHERE id_user = " . $_SESSION['user']['id_user'];
-        $editar_foto_perfil = $conn->query($qeditarfotoperfil);
+
+    // VERIFICAR O TAMANHO DO FICHEIRO
+    if ($_FILES["FotoPerfil"]["size"] > 10240000) {
+        $error_perfil = "O seu ficheiro é demasiado grande (MAX: 10MB).";
+        $uploadOk_perfil = 0;
+    }
+
+    // VERIFICAR A EXTENSAO DO FICHEIRO
+    if ($extensao_perfil != "jpg" && $extensao_perfil != "png" && $extensao_perfil != "jpeg" && $extensao_perfil != "gif") {
+
+        $error_perfil = "Só ficheiros JPG, JPEG, PNG & GIF são permitidos.";
+        $uploadOk_perfil = 0;
+    }
+
+    //VER SE HÁ ERRO
+    if ($uploadOk_perfil == 0) {
+
+        $error_perfil = "O seu ficheiro não foi submetido.";
+    } else {
+        if (move_uploaded_file($_FILES['FotoPerfil']['tmp_name'], $folder_perfil . $novo_ficheiro_perfil)) {
+            $qeditarfotoperfil = "UPDATE users SET profile_image = '$novo_ficheiro_perfil' WHERE id_user = " . $_SESSION['user']['id_user'];
+            $editar_foto_perfil = $conn->query($qeditarfotoperfil);
+        }
     }
 }
+
+$uploadOk_banner = 1;
+$error_banner = '';
 
 if ($imagem_banner != "") {
     $extensao_banner = pathinfo($imagem_banner, PATHINFO_EXTENSION);
     $folder_banner = "img/fotos_banner/";
     $novo_ficheiro_banner = sha1(microtime()) . "." . $extensao_banner;
-    if (move_uploaded_file($_FILES['BannerPerfil']['tmp_name'], $folder_banner . $novo_ficheiro_banner)) {
-        $qeditarbannerperfil = "UPDATE users SET banner_image = '$novo_ficheiro_banner' WHERE id_user = " . $_SESSION['user']['id_user'];
-        $editar_foto_banner = $conn->query($qeditarbannerperfil);
+
+    if ($_FILES["BannerPerfil"]["size"] > 10240000) {
+        $error_banner = "O seu ficheiro é demasiado grande (MAX: 10MB).";
+        $uploadOk_banner = 0;
+    }
+
+    // VERIFICAR A EXTENSAO DO FICHEIRO
+    if ($extensao_banner != "jpg" && $extensao_banner != "png" && $extensao_banner != "jpeg" && $extensao_banner != "gif") {
+
+        $error_banner = "Só ficheiros JPG, JPEG, PNG & GIF são permitidos.";
+        $uploadOk_banner = 0;
+    }
+
+    //VER SE HÁ ERRO
+    if ($uploadOk_banner == 0) {
+
+        $error_banner = "O seu ficheiro não foi submetido.";
+    } else {
+
+        if (move_uploaded_file($_FILES['BannerPerfil']['tmp_name'], $folder_banner . $novo_ficheiro_banner)) {
+            $qeditarbannerperfil = "UPDATE users SET banner_image = '$novo_ficheiro_banner' WHERE id_user = " . $_SESSION['user']['id_user'];
+            $editar_foto_banner = $conn->query($qeditarbannerperfil);
+        }
     }
 }
 
