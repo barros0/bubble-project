6f7278bbab2f37459f4490619f62afd94add8339
barros0 <joaoaobarros7@gmail.com>
2022-06-29 18:48:53 +0100

    Update definicoes_nome.php

--- DWM_2021_22-Prj1_Bubble/Site/src/definicoes/definicoes_nome.php
+++ DWM_2021_22-Prj1_Bubble/Site/src/definicoes/definicoes_nome.php
@@ -3,15 +3,20 @@ require('../../bd.php');
 session_start();
 
 $userid = $_GET['userid'];
-
 $nome = $_REQUEST['nome'];
+
+$erro_nome = "";
 
 if ($nome != "") {
     $stmt_update_nome = $conn->prepare("UPDATE users SET nome = ? WHERE id_user = " . $userid);
     $stmt_update_nome->bind_param('s', $nome);
     $stmt_update_nome->execute();
     $stmt_update_nome->close();
+    $erro_nome = "Nome Atualizado Com Sucesso";
+    array_push($_SESSION['alerts']['errors'], "Nome Atualizado Com Sucesso!");
+} else {
+    $erro_nome = "Introduza Um Nome Valido";
+    array_push($_SESSION['alerts']['errors'], 'Esta FAQ não existe!');
 }
-
 
 header('location:../../definicoes_geral.php');
