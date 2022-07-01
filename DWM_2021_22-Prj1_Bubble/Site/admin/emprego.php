672a25d6be7f3f10e7b8f73ac3643e82a59e0fa7
barros0 <joaoaobarros7@gmail.com>
2022-07-01 13:36:49 +0100

    up

--- DWM_2021_22-Prj1_Bubble/Site/admin/emprego.php
+++ DWM_2021_22-Prj1_Bubble/Site/admin/emprego.php
@@ -15,92 +15,80 @@ if (!isset($emprego)) {
     array_push($_SESSION['alerts']['errors'], 'Este emprego não existe!');
     header('location:./empregos.php');
     exit;
-    
 }
 
 ?>
 
 <div class="s-container">
-    <form class="form-control" method="post" enctype="multipart/form-data" action="./update_emprego.php?empregoid=<?= $empregoid ?>"
-          autocomplete="off">
+    <form class="form-control empregos-form" method="post" enctype="multipart/form-data" action="./update_emprego.php?empregoid=<?= $empregoid ?>" autocomplete="off">
 
         <div class="title">
             <h2>Atualizar Emprego</h2>
         </div>
 
         <div class="buttons_post btnfrm">
-                    <span class="addft">Adicionar Foto</span>
-                    <div class="upload_img" onchange="verFoto()">
-                        <button class="upload_btn "><i class='bx bx-plus'></i></button>
-                        <input id="input_file_emp" value="<?= $imagem['foto'] ?>" type="file" name="foto_emprego">
-                    </div>
+            <span class="addft">Adicionar Foto</span>
+            <div class="upload_img" onchange="verFoto()">
+                <button class="upload_btn "><i class='bx bx-plus'></i></button>
+                <input id="input_file_emp" value="<?= $imagem['foto'] ?>" type="file" name="foto_emprego">
+            </div>
 
-                </div>
+        </div>
 
         <div class="form-group">
             <label for="titulo">Título</label>
-            <input name="titulo" value="<?= $emprego['titulo'] ?>" type="text" class="form-control" id="titulo"
-                   placeholder="Título" required>
+            <input name="titulo" value="<?= $emprego['titulo'] ?>" type="text" class="form-control" id="titulo" placeholder="Título" required>
         </div>
 
         <div class="form-group">
             <label for="qualificacoes">Qualificações</label>
-            <input name="qualificacoes" value="<?= $emprego['qualificacoes'] ?>" type="text" class="form-control" id="qualificacoes"
-                   placeholder="Qualificações" required>
+            <input name="qualificacoes" value="<?= $emprego['qualificacoes'] ?>" type="text" class="form-control" id="qualificacoes" placeholder="Qualificações" required>
         </div>
 
         <div class="form-group">
             <label for="experiencia">Experiência</label>
-            <input name="experiencia" value="<?= $emprego['experiencia'] ?>" type="text" class="form-control" id="experiencia"
-                   placeholder="Experiência" required>
+            <input name="experiencia" value="<?= $emprego['experiencia'] ?>" type="text" class="form-control" id="experiencia" placeholder="Experiência" required>
         </div>
 
         <div class="form-group">
             <label for="requisitos">Requisitos</label>
-            <input name="requisitos" value="<?= $emprego['requisitos'] ?>" type="text" class="form-control" id="requisitos"
-                   placeholder="Requisitos" required>
+            <input name="requisitos" value="<?= $emprego['requisitos'] ?>" type="text" class="form-control" id="requisitos" placeholder="Requisitos" required>
         </div>
 
         <div class="form-group">
             <label for="vagas">Número de Vagas</label>
-            <input name="vagas" value="<?= $emprego['vagas'] ?>" type="number" class="form-control" id="vagas"
-                   placeholder="Número de Vagas" required>
+            <input name="vagas" value="<?= $emprego['vagas'] ?>" type="number" class="form-control" id="vagas" placeholder="Número de Vagas" required>
         </div>
 
         <div class="form-group">
             <label for="localizacao">Localização</label>
-            <input name="localizacao" value="<?= $emprego['localizacao'] ?>" type="text" class="form-control" id="localizacao"
-                   placeholder="Localização" required>
+            <input name="localizacao" value="<?= $emprego['localizacao'] ?>" type="text" class="form-control" id="localizacao" placeholder="Localização" required>
         </div>
 
         <div class="form-group">
             <label for="horario">Horários</label>
-            <input name="horario" value="<?= $emprego['horario'] ?>" type="text" class="form-control" id="horario"
-                   placeholder="Horários" required>
+            <input name="horario" value="<?= $emprego['horario'] ?>" type="text" class="form-control" id="horario" placeholder="Horários" required>
         </div>
 
         <div class="form-group">
             <label for="tipo">Tipo</label>
-            <input name="tipo" value="<?= $emprego['tipo'] ?>" type="text" class="form-control" id="tipo"
-                   placeholder="Tipo" required>
+            <input name="tipo" value="<?= $emprego['tipo'] ?>" type="text" class="form-control" id="tipo" placeholder="Tipo" required>
         </div>
 
         <div class="form-group">
             <label for="categoria">Categoria</label>
-            <input name="categoria" value="<?= $emprego['categoria'] ?>" type="text" class="form-control" id="categoria"
-                   placeholder="Categoria" required>
+            <input name="categoria" value="<?= $emprego['categoria'] ?>" type="text" class="form-control" id="categoria" placeholder="Categoria" required>
         </div>
 
-        
+
         <div class="form-group">
             <label for="descricao">Descrição</label>
-            <input name="descricao" value="<?= $emprego['descricao'] ?>" type="text" class="form-control" id="descricao"
-                   placeholder="Descrição" required>
+            <input name="descricao" value="<?= $emprego['descricao'] ?>" type="text" class="form-control" id="descricao" placeholder="Descrição" required>
         </div>
 
 
         <a class="btn btn-danger" href="./update_emprego.php?delete_empregoid=<?= $empregoid  ?>">Eliminar</a>
-            <button type="submit" class="btn btn-primary">Gravar</button>
+        <button type="submit" class="btn btn-primary">Gravar</button>
 
     </form>
 
