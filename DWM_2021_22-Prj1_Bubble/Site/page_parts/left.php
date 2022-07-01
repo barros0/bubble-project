3dbf3f4ce627bbc85562125460d2b27e21f74ef1
Assado3000 <dev.andre.sousa@gmail.com>
2022-07-01 01:21:12 +0100

    ewew
    ewew

--- DWM_2021_22-Prj1_Bubble/Site/page_parts/left.php
+++ DWM_2021_22-Prj1_Bubble/Site/page_parts/left.php
@@ -17,7 +17,6 @@ function active_left($currect_page)
             <li><a class="<?php active_left('.php'); ?>" href=""><i class='bx bx-group'></i>Amigos</a></li>
             <li><a class="<?php active_left('marketplace.php'); ?>" href="marketplace.php"><i class='bx bx-store-alt'></i>Marketplace</a></li>
             <li><a class="<?php active_left('empregos.php'); ?>" href="empregos.php"><i class='bx bxs-megaphone'></i>Oferta de Emprego</a></li>
-            <li><a class="<?php active_left('empregosUtilizador.php'); ?>" href="empregosUtilizador.php"><i class='bx bxs-megaphone'></i>Gerir Ofertas</a></li>
             <li><a class="<?php active_left('eventos.php'); ?>" href="eventos.php"><i class='bx bx-calendar-event'></i>Eventos</a></li>
             <li><a class="<?php active_left('faqs.php'); ?>" href="faqs.php"><i class='bx bx-question-mark'></i>FAQS</a></li>
         </ul>
