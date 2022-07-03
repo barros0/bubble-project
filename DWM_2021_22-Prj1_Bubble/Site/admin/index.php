94ac8625ecba7b80ec11670263682e7d7b2461af
MicaG28 <65852191+MicaG28@users.noreply.github.com>
2022-07-03 16:26:54 +0100

    admin graph

--- DWM_2021_22-Prj1_Bubble/Site/admin/index.php
+++ DWM_2021_22-Prj1_Bubble/Site/admin/index.php
@@ -5,7 +5,7 @@ $c_users = $conn->query('select count(*) as total from users')->fetch_assoc()['t
 $c_publicacoes = $conn->query('select count(*) as total from publicacoes')->fetch_assoc()['total'];
 $c_users = $conn->query('select count(*) as total from users')->fetch_assoc()['total'];
 
-$conn->close();
+
 
 
 // files
@@ -44,6 +44,66 @@ $site_total_espaco =  converteTamanho(tamanhoPasta('../'));
 $imagens_espaco =  converteTamanho(tamanhoPasta('../img'));
 $total_espaco =  converteTamanho(disk_total_space('../'));
 $videos_espaco =  converteTamanho(tamanhoPasta('../videos'));
+
+
+
+/*
+ graficos users
+*/
+$users = $conn->query('select * from users inner join estados_users on users.estado = estado_user_id');
+
+// get all generos
+$generosq = $conn->query('select * from generos');
+
+// passa generos para array associativo com nome e quantidade de users que tem esse geneto
+foreach ($generosq as $key => $genero) {
+    $valoresgeneros[$key] = $conn->query('select count(*) as total from users where genero =' . $genero['genero_id'])->fetch_assoc()['total'];
+}
+foreach ($generosq as $key => $genero) {
+    $generos[$genero['genero_id']] = $genero['nome_genero'];
+}
+
+/// get all nacionaliadades
+$nacionalidadesq = $conn->query('select * from nacionalidades');
+
+/// passaklas por array associativo e faz count de quantos axistem com essa nacionalidade
+foreach ($nacionalidadesq as $key => $nacionalidade) {
+    $nacionalidades[$nacionalidade['pais']] = $conn->query('select count(*) as total from users where nacionalidade =' . $nacionalidade['nacionalidade_id'])->fetch_assoc()['total'];
+}
+
+$idades = array();
+// conta a idade do usser
+function getidade($data)
+{
+    $data = new DateTime($data);
+    $atual = new DateTime();
+    $dif = $atual->diff($data);
+    return $dif->y;
+}
+
+// para cada user
+foreach ($users as $key => $user) {
+
+    // conta a idade para o user
+    $idade = getidade($user['data_nascimento']);
+
+    // ve o seu genero
+    $genero = $generos[$user['genero']];
+
+    // e para cada idade do genero do user adiciona mais um
+    if (empty($idades[$idade])) {
+        $idades[$genero][$idade] = 1;
+    } else {
+        $idades[$genero][$idade] = $idades[$idade]++;
+    }
+}
+
+// para os generos que nao tem users coloca 0 para evitar erros
+foreach ($generos as $key => $genero) {
+    if (empty($idades[$genero])) {
+        $idades[$genero] = [0];
+    }
+}
 
 
 ?>
--- DWM_2021_22-Prj1_Bubble/Site/admin/users.php
+++ DWM_2021_22-Prj1_Bubble/Site/admin/users.php
@@ -5,59 +5,6 @@ require_once('./partials/header.php');
 // get al users
 $users = $conn->query('select * from users inner join estados_users on users.estado = estado_user_id');
 
-// get all generos
-$generosq = $conn->query('select * from generos');
-
-// passa generos para array associativo com nome e quantidade de users que tem esse geneto
-foreach ($generosq as $key => $genero) {
-    $valoresgeneros[$key] = $conn->query('select count(*) as total from users where genero =' . $genero['genero_id'])->fetch_assoc()['total'];
-}
-foreach ($generosq as $key => $genero) {
-    $generos[$genero['genero_id']] = $genero['nome_genero'];
-}
-
-/// get all nacionaliadades
-$nacionalidadesq = $conn->query('select * from nacionalidades');
-
-/// passaklas por array associativo e faz count de quantos axistem com essa nacionalidade
-foreach ($nacionalidadesq as $key => $nacionalidade) {
-    $nacionalidades[$nacionalidade['pais']] = $conn->query('select count(*) as total from users where nacionalidade =' . $nacionalidade['nacionalidade_id'])->fetch_assoc()['total'];
-}
-
-$idades = array();
-// conta a idade do usser
-function getidade($data)
-{
-    $data = new DateTime($data);
-    $atual = new DateTime();
-    $dif = $atual->diff($data);
-    return $dif->y;
-}
-
-// para cada user
-foreach ($users as $key => $user) {
-
-    // conta a idade para o user
-    $idade = getidade($user['data_nascimento']);
-
-    // ve o seu genero
-    $genero = $generos[$user['genero']];
-
-    // e para cada idade do genero do user adiciona mais um
-    if (empty($idades[$idade])) {
-        $idades[$genero][$idade] = 1;
-    } else {
-        $idades[$genero][$idade] = $idades[$idade]++;
-    }
-}
-
-// para os generos que nao tem users coloca 0 para evitar erros
-foreach ($generos as $key => $genero) {
-    if (empty($idades[$genero])) {
-        $idades[$genero] = [0];
-    }
-}
-
 
 ?>
 
@@ -78,6 +25,7 @@ foreach ($generos as $key => $genero) {
                     <th scope="col">Nome</th>
                     <th scope="col">Email</th>
                     <th scope="col">Estado</th>
+                    <th scope="col">Eliminar</th>
                     <th scope="col">Editar</th>
                 </tr>
             </thead>
@@ -96,8 +44,14 @@ foreach ($generos as $key => $genero) {
                                     <?= $user['nome_estado_user'] ?>
                                 </span></p>
                         </td>
+
                         <td>
-                            <a href="./user.php?userid=<?= $user['id_user'] ?>">
+                            <a class="text-center btn btn-danger" href="./users.php">
+                                <i class="fa fa-trash"></i>
+                            </a>
+                        </td>
+                        <td>
+                            <a class="text-center" href="./user.php?userid=<?= $user['id_user'] ?>">
                                 <i class="fa fa-pen"></i>
                             </a>
                         </td>
