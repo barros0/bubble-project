708cfee56a70179206211cc3354f7765bf0c4a59
João Duarte <joao.bartolomeu.tic@gmail.com>
2022-07-06 01:11:59 +0100

    up mrk

--- DWM_2021_22-Prj1_Bubble/Site/add_marketplace.php
+++ DWM_2021_22-Prj1_Bubble/Site/add_marketplace.php
@@ -2,7 +2,7 @@
 require('./bd.php');
 session_start();
 
-
+$user_id = $_SESSION['user']['id_user'];
 $titulo = $_REQUEST['titulo_market_textarea'];
 $descricao = $_REQUEST['descricao_market_textarea'];
 $preco = $_REQUEST['preco_market_textarea'];
@@ -14,8 +14,13 @@ $folder_marketplace = "img/marketplace/";
 $novo_ficheiro_market = sha1(microtime()) . "." . $extensao;
 
 if ($titulo != "" && $preco != "" && $descricao != "" && move_uploaded_file($_FILES['foto_market']['tmp_name'], $folder_marketplace . $novo_ficheiro_market)) {
-    $qmarket = "INSERT INTO marketplace (titulo,descricao,preco,imagem) VALUES ('" . $titulo . "','" . $descricao . "','" . $preco . "','" . $novo_ficheiro_market . "')";
-    $market = $conn->query($qmarket);
+    $market = $conn->prepare("INSERT INTO marketplace (titulo,descricao,preco,imagem,id_user) VALUES (?,?,?,?,?)");
+    $market->bind_param("ssssi",$titulo,$descricao,$preco,$novo_ficheiro_market,$user_id);
+    $market->execute();
+
+    array_push($_SESSION['alerts']['success'], 'Produto Adicionado Com Sucesso');
+    $market->close();
+    header('location:marketplace.php');
 }
 
 header('location:marketplace.php');
--- DWM_2021_22-Prj1_Bubble/Site/estrutura_marketplace.php
+++ DWM_2021_22-Prj1_Bubble/Site/estrutura_marketplace.php
@@ -1,24 +1,23 @@
 <?php
-$marketplace = "select * from marketplace";
+$marketplace =  $conn->query("select * from marketplace");
 
-$result_set = $conn->query($marketplace);
+$result_set =($marketplace);
 ?>
 
 <?php
 while ($market = $result_set->fetch_assoc()) {
+    $user = $conn->query("select username from users where id_user =".$market['id_user'])->fetch_assoc();
+
 ?>
     <div class="market">
   
         <div class="market_left">
-            <div class="data_market">
-                <span class="dia/mes"></span>
-                <span class="hora"></span>
-            </div>
             <div class="imagem_market">
             <img class="market_right" src="img/marketplace/<?php  echo $market['imagem'] ?>" alt="foto_market">
             </div>
+        <div class="seccao2-caixamarket">
             <div class="autor_market">
-                <p>Autor: <?php echo $market['id_user'] ?></p>
+                <p>Autor: <?php echo $user['username'] ?></p>
             </div>
             <div class="titulo_market">
                 <p><?php echo $market['titulo'] ?></p>
@@ -35,6 +34,7 @@ while ($market = $result_set->fetch_assoc()) {
             <div class="texto-download">
                 <p><i class="fa-solid fa-download"></i>1.6k Download</p>
             </div>
+        </div>
         </div>
     </div>
 <?php
