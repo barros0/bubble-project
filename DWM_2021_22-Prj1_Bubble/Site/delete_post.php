5bde2ce3b11a4173591511af79274db7c4d570d7
barros0 <joaoaobarros7@gmail.com>
2022-07-07 16:44:16 +0100

    update

--- DWM_2021_22-Prj1_Bubble/Site/delete_post.php
+++ DWM_2021_22-Prj1_Bubble/Site/delete_post.php
@@ -6,13 +6,21 @@ $estado_eliminada = 2;
 $id_publicacao = $_GET['id_pub'];
 $id_user_session = $_SESSION['user']['id_user'];
 
-$dono_pub = $conn->query("select * from publicacoes where publicacao_id=" . $id_publicacao)->fetch_assoc();
+$dono_pub = $conn->prepare("select * from publicacoes where publicacao_id= ?");
+$dono_pub->bind_param("i", $id_publicacao);
+$dono_pub->execute();
+$result = $dono_pub->get_result();
+$result = $result->fetch_assoc();
+
 //proteção
-if ($dono_pub['user_id'] === $id_user_session) {
-    $delete_post = $conn->query("UPDATE publicacoes SET estado_pub = '$estado_eliminada' WHERE publicacao_id = " . $id_publicacao);
-    $delete_post = $conn->close();
+if ($result['user_id'] == $id_user_session) {
+    $delete_post = $conn->prepare("UPDATE publicacoes SET estado_pub = ? WHERE publicacao_id = ?");
+    $delete_post->bind_param("ii", $estado_eliminada, $id_publicacao);
+    $delete_post->execute();
     array_push($_SESSION['alerts']['success'], "Eliminado com sucesso!");
+    $delete_post->close();
 } else {
     array_push($_SESSION['alerts']['errors'], "Não podes eliminar uma publicação que não seja tua!");
 }
+
 header('location:feed.php');
