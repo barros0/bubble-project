5661c772b0d0c28361564752f4f2fcd59838398e
barros0 <joaoaobarros7@gmail.com>
2022-07-08 00:59:57 +0100

    update

--- DWM_2021_22-Prj1_Bubble/Site/page_parts/left.php
+++ DWM_2021_22-Prj1_Bubble/Site/page_parts/left.php
@@ -14,11 +14,22 @@ function active_left($currect_page)
         <ul>
             <li><a class="verperfil" href="perfil.php?username=<?= $userq['username'] ?>"><img class="avatar" src="img/fotos_perfil/<?php echo $userq['profile_image'] ?>" alt="fotoperfil"><?php echo $userq['nome'] ?></a>
             </li>
-            <li><a class="<?php active_left('conexoes.php'); ?>" href="conexoes.php"><i class='bx bx-group'></i>Conexões</a></li>
-            <li><a class="<?php active_left('marketplace.php'); ?>" href="marketplace.php"><i class='bx bx-store-alt'></i>Marketplace</a></li>
-            <li><a class="<?php active_left('empregos.php'); ?>" href="empregos.php"><i class='bx bxs-megaphone'></i>Oferta de Emprego</a></li>
-            <li><a class="<?php active_left('eventos.php'); ?>" href="eventos.php"><i class='bx bx-calendar-event'></i>Eventos</a></li>
-            <li><a class="<?php active_left('faqs.php'); ?>" href="faqs.php"><i class='bx bx-question-mark'></i>FAQS</a></li>
+            <li>
+                <a class="<?php active_left('conexoes_seguidores.php');
+                            active_left('conexoes_aseguir.php'); ?>" href="conexoes_seguidores.php"><i class='bx bx-group'></i>Conexões</a>
+            </li>
+            <li>
+                <a class="<?php active_left('marketplace.php'); ?>" href="marketplace.php"><i class='bx bx-store-alt'></i>Marketplace</a>
+            </li>
+            <li>
+                <a class="<?php active_left('empregos.php'); ?>" href="empregos.php"><i class='bx bxs-megaphone'></i>Oferta de Emprego</a>
+            </li>
+            <li>
+                <a class="<?php active_left('eventos.php'); ?>" href="eventos.php"><i class='bx bx-calendar-event'></i>Eventos</a>
+            </li>
+            <li>
+                <a class="<?php active_left('faqs.php'); ?>" href="faqs.php"><i class='bx bx-question-mark'></i>FAQS</a>
+            </li>
         </ul>
 
     </div>
