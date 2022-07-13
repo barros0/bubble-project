17b253a0a4415a06532c42ad18da3812447313e7
MicaG28 <65852191+MicaG28@users.noreply.github.com>
2022-07-13 11:22:17 +0100

    resultados

--- DWM_2021_22-Prj1_Bubble/Site/pesquisa.php
+++ DWM_2021_22-Prj1_Bubble/Site/pesquisa.php
@@ -61,7 +61,7 @@ foreach ($users as $user) {
     $user_arr = [
         'titulo' => $user['username'],
         'subtitulo' => $user['username'],
-        'tipo' => 'user',
+        'tipo' => 'pessoa',
         //'img' => './img/publicacoes/'.$conn->query('select * from publicacoes_fotos where publicacao_id ='.$pub['publicacao_id'])->fetch_assoc()['caminho'],
         'link' => './perfil?username=' . $user['username'],
     ];
@@ -130,145 +130,174 @@ foreach ($market as $mark) {
                 </a>
             </div>
 
-            <div class="col-12 notificacoes-wrapper">
-                <div class="col-12">
-                    <h1>Resultados para: <?= $value_search ?></h1>
-                </div>
+            <div class="col-12 resultados-para">
+                <h1>Resultados para: <?= $value_search ?></h1>
+                <hr>
+            </div>
 
-                <div class="resultados">
-                    <?php
-                    if ($resultados) {
-                        foreach ($resultados as $resultado) { ?>
-                            <div class="pesquisa">
-                                <a href="<?= $resultado['link'] ?>">
-                                    <div class="d-flex">
-                                        <div class="img-radius">
-                                            <img src="https://picsum.photos/200/300" alt="">
+            <div class="resultados">
+                <?php
+                if ($resultados) {
+                    foreach ($resultados as $resultado) { ?>
+                        <div class="pesquisa">
+                            <a href="<?= $resultado['link'] ?>">
+                                <div class="d-flex">
+                                    <div class="img-radius">
+                                        <img src="https://picsum.photos/200/300" alt="">
+                                    </div>
+                                    <div class="info">
+                                        <div class="titulo">
+                                            <h2><?php echo $resultado['titulo'] ?></h2>
                                         </div>
-                                        <div class="info">
-                                            <div class="titulo">
-                                                <h2><?php echo $resultado['titulo'] ?></h2>
-                                            </div>
-                                            <div class="desc">
-                                                <p><?php echo $resultado['tipo'] ?></p>
-                                            </div>
+                                        <div class="desc">
+                                            <p><?php echo $resultado['tipo'] ?></p>
                                         </div>
                                     </div>
-                                </a>
-                            </div>
-                        <?php }
-                    } ?>
-                </div>
+                                </div>
+                            </a>
+                        </div>
+                    <?php }
+                } ?>
             </div>
-            <?php
-
-
-            ?>
-            <style>
-
-
-                .center {
-                    display: flex;
-                    width: 70%;
-                    margin-top: 98px;
-                    margin-right: 70px;
-                    flex-direction: column;
-                }
-
-                .filtros {
-                    color: var(--white);
-                    margin-left: 10px;
-                }
-
-                .actions {
-                    display: flex;
-                    width: 100%;
-                    height: 70px;
-                    justify-content: space-between;
-                    margin-bottom: 20px;
-                }
-
-                .bt_op {
-                    background-color: #404040;
-                    border-bottom: 2px solid transparent;
-                    width: 49%;
-                    border-radius: 10px;
-                    height: 100%;
-                    display: flex;
-                    flex-direction: column;
-                    align-items: center;
-                    justify-content: center;
-                    text-align: center;
-                    padding: 10px;
-                    text-decoration: none;
-                    margin: 10px;
-                }
-
-                .bt_op:hover, .bt_op:hover {
-                    background-color: rgb(88, 88, 88);
-                }
-
-                .white {
-                    color: white;
-                }
-
-
-                .active_bt {
-                    border-bottom: 2px solid #00ff8a;
-                    border-bottom-right-radius: 10px;
-                }
-
-                .active_bt .white {
-                    color: var(--verde) !important;
-                }
-
-
-                .pesquisa {
-                    background-color: var(--background);
-                    border-radius: 6px;
-                    padding: 10px;
-                    margin-bottom: 4px;
-                }
-
-                .pesquisa .info {
-                    padding-top: 6px;
-                    padding-left: 16px;
-                }
-
-                .pesquisa .titulo h2 {
-                    font-size: 20px;
-                    font-weight: bold;
-                    text-decoration: none;
-                    color: var(--palavras);
-                    margin-top: 10px;
-                }
-
-                .pesquisa .titulo a:hover {
-                    color: var(--verde-hover);
-                }
-
-                .pesquisa .titulo * {
-                    font-size: 14px;
-                    color: var(--palavras);
-                }
-
-                .pesquisa .img-radius img {
-                    border-radius: 50%;
-                    width: 80px;
-                    height: 80px;
-                    object-fit: cover;
-                    background-color: var(--background);
-                }
-
-                .notificacoes p, h1 {
-                    color: var(--palavras);
-                }
-            </style>
-
-
-
-
         </div>
+        <?php
+
+
+        ?>
+        <style>
+
+
+            .center {
+                display: flex;
+                width: 70%;
+                margin-top: 98px;
+                margin-right: 70px;
+                flex-direction: column;
+            }
+
+            .filtros {
+                color: var(--white);
+                margin-left: 10px;
+            }
+
+            .actions {
+                display: flex;
+                width: 100%;
+                height: 70px;
+                justify-content: space-between;
+                margin-bottom: 20px;
+            }
+
+            .bt_op {
+                background-color: #404040;
+                border-bottom: 2px solid transparent;
+                width: 49%;
+                border-radius: 10px;
+                height: 100%;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                text-align: center;
+                padding: 10px;
+                text-decoration: none;
+                margin: 10px;
+            }
+
+            .bt_op:hover, .bt_op:hover {
+                background-color: rgb(88, 88, 88);
+            }
+
+            .white {
+                color: white;
+            }
+
+            .pesquisa a {
+                text-decoration: none;
+            }
+
+            .pesquisa .desc {
+                color: var(--verde-hover);
+                text-transform: capitalize;
+            }
+
+
+            .active_bt {
+                border-bottom: 2px solid #00ff8a;
+                border-bottom-right-radius: 10px;
+            }
+
+            .active_bt .white {
+                color: var(--verde) !important;
+            }
+
+            .resultados-para{
+                margin-top: 10px;
+                margin-bottom: 10px;
+                padding-bottom: 10px;
+                text-align: center;
+            }
+
+            .resultados-para hr{
+                margin: auto;
+                width: 250px;
+                margin-top: 5px;
+                height: 3px;
+                background-color: var(--verde-hover);
+            }
+
+            .pesquisa {
+                background-color: var(--parcelas);
+                border-radius: 8px;
+                padding: 10px;
+                margin-bottom: 10px;
+            }
+
+            .pesquisa:hover {
+                background-color: var(--items);
+            }
+
+            .resultados {
+                padding: 10px;
+            }
+
+            .pesquisa .info {
+                padding-top: 6px;
+                padding-left: 16px;
+            }
+
+            .pesquisa .titulo h2 {
+                font-size: 20px;
+                font-weight: bold;
+                text-decoration: none;
+                color: var(--palavras);
+                margin-top: 10px;
+            }
+
+            .pesquisa .titulo a:hover {
+                color: var(--verde-hover);
+            }
+
+            .pesquisa .titulo * {
+                font-size: 14px;
+                color: var(--palavras);
+            }
+
+            .pesquisa .img-radius img {
+                border-radius: 50%;
+                width: 80px;
+                height: 80px;
+                object-fit: cover;
+                background-color: var(--background);
+            }
+
+            .notificacoes p, h1 {
+                color: var(--palavras);
+            }
+        </style>
+
+
+    </div>
     </div>
 
     <div class="right">
