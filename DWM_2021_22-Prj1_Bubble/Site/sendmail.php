f474bd2a7131a820881e59a5424babd2fda5f9cc
MicaG28 <65852191+MicaG28@users.noreply.github.com>
2022-07-14 12:30:00 +0100

    mail

--- DWM_2021_22-Prj1_Bubble/Site/sendmail.php
+++ DWM_2021_22-Prj1_Bubble/Site/sendmail.php
@@ -1,27 +1,73 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require './vendor/phpmailer/phpmailer/src/Exception.php';
+require './vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require './vendor/phpmailer/phpmailer/src/SMTP.php';
+
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
 
 function confirmaremail($user, $token){
-$to = $user['email'];
-$subject = "Confirme o seu email | BUBBLE";
+    $mail = new PHPMailer(true);
 
-$message = 'Bem-vindo '.$user['nome'].',<br>';
-$message .= "Para confirmar a sua conta clique no link que se segue abaixo:<br><br>";
-$message .= "<a href='/confirmar-email/".$token."'></a> ,<br>";
+    try {
+        //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'mail.social-bubble.pt';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'noreply@social-bubble.pt';                     //SMTP username
+        $mail->Password   = 'auto_social';                               //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-// Always set content-type when sending HTML email
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        //Recipients
+        $mail->setFrom('submica99@gmail.com', 'Mailer');
+        $mail->addAddress('submica99@gmail.com', 'Joe User');     //Add a recipient
+        /* $mail->addAddress('ellen@example.com');               //Name is optional
+         $mail->addReplyTo('info@example.com', 'Information');
+         $mail->addCC('cc@example.com');
+         $mail->addBCC('bcc@example.com');*/
 
-// More headers
-$headers .= 'From: <enquiry@example.com>' . "\r\n";
-$headers .= 'Cc: myboss@example.com' . "\r\n";
+        //Attachments
+        /*$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+        $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+*/
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = 'Bubble | Confirma o teu endereço e-mail';
+        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+        $mail->AltBody = 'É necessário confirmares o teu e-mail antes de fazeres login :)';
 
-$email = mail($to,$subject,$message,$headers);
-
-    if( $email == true ) {
-       //sucesso
-    }else {
-//fail
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
+}
 
+//confirmaremail(1,1);
+
+
+
+function gerar_link_confirm_email($token){
+    if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+        $url = "https://";
+    else
+        $url = "http://";
+// Append the host(domain name, ip) to the URL.
+    $url.= $_SERVER['HTTP_HOST'];
+
+// Append the requested resource location to the URL
+    $url.= $_SERVER['REQUEST_URI'];
+
+    $url = explode('/', $url);
+    array_pop($url);
+    $link =  implode('/', $url).'/confirmar_email.php?token='.$token;
+    return;
 }
