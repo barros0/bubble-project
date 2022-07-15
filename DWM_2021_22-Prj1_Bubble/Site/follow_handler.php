866ea069b7933d082694e43c162cf02446e03eef
MicaG28 <65852191+MicaG28@users.noreply.github.com>
2022-07-15 14:47:09 +0100

    tyt

--- DWM_2021_22-Prj1_Bubble/Site/follow_handler.php
+++ DWM_2021_22-Prj1_Bubble/Site/follow_handler.php
@@ -1,5 +1,6 @@
 <?php
 require('./bd.php');
+require 'functions_handler.php';
 session_start();
 
 $userid = $_SESSION['user']['id_user'];
@@ -12,6 +13,8 @@ if (isset($_GET['follow'])) {
     $follow->bind_param("ii", $userid, $followid);
     $follow->execute();
 
+    notf_seguir($followid, $follow->insert_id);
+
     header('Location: ' . $_SERVER['HTTP_REFERER']);
 }
 
@@ -19,9 +22,17 @@ if (isset($_GET['follow'])) {
 if (isset($_GET['remove-follow'])) {
     $followid = $_GET['remove-follow'];
 
+    $get_follow = $conn->prepare('select * from seguir where id_seguidor = ? and id_utilizador = ?');
+    $get_follow->bind_param("ii", $userid, $followid);
+    $get_follow->execute();
+    $seguir_id = $get_follow->get_result()->fetch_assoc()['id_seguir'];
+    $get_follow->close();
+
     $delete_follow = $conn->prepare('delete from seguir where id_seguidor = ? and id_utilizador = ?');
     $delete_follow->bind_param("ii", $userid, $followid);
     $delete_follow->execute();
+
+    notf_remover_seguir($seguir_id,$conn);
 
     header('Location: ' . $_SERVER['HTTP_REFERER']);
 }
