0c1f04bda7e27d5a2cee72b43e20b5b40d108553
João Duarte <joao.bartolomeu.tic@gmail.com>
2022-07-15 17:46:20 +0100

    up login

--- DWM_2021_22-Prj1_Bubble/Site/dologin.php
+++ DWM_2021_22-Prj1_Bubble/Site/dologin.php
@@ -6,9 +6,53 @@ session_start();
 if (isset($_POST['email']) && isset($_POST['password'])) {
     $email = $_POST["email"];
     $password = $_POST["password"];
+    
 
     $query_email = "Select * from users where email='" . $email . "'";
     $user = $conn->query($query_email)->fetch_assoc();
+
+    $estado = $user["estado"];
+    
+    if($estado != 2){
+        
+        switch($estado){
+            case 1:
+                echo '<h2>Confirme o seu e-mail</h2>';
+                array_push($_SESSION['alerts']['errors'], 'Confirme o seu e-mail');
+                header('location:./login.php');
+                exit;
+            break;
+
+            case 3:
+                echo '<h2>Conta Desativada</h2>';
+                array_push($_SESSION['alerts']['errors'], 'Conta Desativada');
+                header('location:./login.php');
+                exit;
+            break;
+            
+            case 4:
+                echo '<h2>Estado Conta: Suspensa</h2>';
+                array_push($_SESSION['alerts']['errors'], 'Estado Conta: Suspensa');
+                header('location:./login.php');
+                exit;
+            break;
+
+            case 5:
+                echo '<h2>Estado Conta: Banido</h2>';
+                array_push($_SESSION['alerts']['errors'], 'Estado Conta: Banido');
+                header('location:./login.php');
+                exit;
+            break;
+        }
+
+        exit;
+
+    }
+
+
+
+
+
 
     //se o user nao existir
     if (!isset($user)) {
