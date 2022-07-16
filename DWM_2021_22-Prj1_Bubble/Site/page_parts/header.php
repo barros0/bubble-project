6e1c844c2e050551d6b14bb16b40af2771ed9926
barros0 <joaoaobarros7@gmail.com>
2022-07-16 09:21:21 +0100

    up

--- DWM_2021_22-Prj1_Bubble/Site/page_parts/header.php
+++ DWM_2021_22-Prj1_Bubble/Site/page_parts/header.php
@@ -148,7 +148,7 @@ $historico = $conn->query('select * from historico_pesquisa where id_utilizador 
         <div class="wrapper_searchbar">
             <form class="form_searchbar" method="get" action="pesquisa.php">
                 <div class="div_input_searchbar">
-                    <input id="input_searchbar" type="text" class="input_searchbar" placeholder="Search here..." name="search" required="required" value="" />
+                    <input id="input_searchbar" type="text" class="input_searchbar" placeholder="Pesquise Aqui..." name="search" required="required" value="" />
                     <span class="input_search_group_btn">
                         <button class="button_searchbar"><i class='bx bx-search'></i></button>
                     </span>
